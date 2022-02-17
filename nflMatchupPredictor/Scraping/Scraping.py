93aaf60e21d7b6c4957e5581a595d8f8277106e1
Jonathan Armitage <jarmitage@developertown.com>
2022-02-17 14:16:24 -0500

    Scrape schedule

--- nflMatchupPredictor/Scraping/Scraping.py
+++ nflMatchupPredictor/Scraping/Scraping.py
@@ -1,18 +1,27 @@
 import warnings
+from collections import Counter
 
 import numpy as np
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+from nflMatchupPredictor.DataLoaders.DataLoader import DataLoader
+
 warnings.simplefilter("ignore")
 
 
 class Scraping:
-    def __init__(self, verbose=False):
+    def __init__(self, nfl_year=None, verbose=False):
+        self.nfl_year = nfl_year
         self.verbose = verbose
 
         self.base_url = "https://www.pro-football-reference.com"
+
+    def finish_url(self):
+        dt = {"by_team": "/teams/{}/{}.htm", "by_schedule": "/years/{}/games.htm"}
+
+        return dt
 
     def feature_helper(self):
         feat_dt = {
@@ -32,17 +41,85 @@ class Scraping:
 
         return dt
 
+    def counter_helper(self, data_object):
+        cols = ["winner_tie", "loser_tie", "home_team_name"]
+        tmp_data_object = data_object.loc[:, cols]
+        tmp_data_object.reset_index(drop=True, inplace=True)
+
+        a_team = []
+        for i in range(tmp_data_object.shape[0]):
+            tmp_counter = Counter(tmp_data_object.iloc[i].tolist())
+            for tm, item in tmp_counter.items():
+                if item == 1:
+                    a_team.append(tm)
+
+        return a_team
+
     def win_loss_helper(self):
         win_loss_dt = {"W": 1, "L": 0, "T": 0.5, "bye_week": 0}
         return win_loss_dt
 
-    def make_request(self, team_abbr, year):
-        update_url = self.base_url + f"/teams/{team_abbr}/{year}.htm"
+    def make_request(self, team_abbr=None):
+        if team_abbr:
+            # update_url = self.base_url + f"/teams/{team_abbr}/{self.nfl_year}.htm"
+            update_url = self.base_url + self.finish_url().get("by_team").format(
+                self.nfl_year
+            )
+        else:
+            update_url = self.base_url + self.finish_url().get("by_schedule").format(
+                self.nfl_year
+            )
         return requests.get(update_url)
 
-    def make_soup(self, team_abbr, year):
-        req = self.make_request(team_abbr=team_abbr, year=year)
+    def make_soup(self, team_abbr=None):
+        req = self.make_request(team_abbr=team_abbr)
         return BeautifulSoup(req.content, "lxml")
+
+    def get_boxscore_url(self, soup_object):
+        td_center = soup_object.select("td.center")
+
+        bx = []
+        for i in td_center:
+            try:
+                f_href = i.find("a").get("href")
+                bx.append(f_href)
+            except AttributeError:
+                bx.append("no_data")
+
+        tm_abbrev = []
+        for item in bx:
+            if item != "no_data":
+                len_item = len(item)
+                f_item = item[(len_item - 7) :]
+                tm_abbrev.append(f_item.split(".")[0])
+            else:
+                tm_abbrev.append("no_data")
+
+        return tm_abbrev
+
+    def get_schedule(self, soup_object):
+        html_table = soup_object.select("table#games")[0]
+        html_to_df = pd.read_html(str(html_table))[0]
+        html_to_df = html_to_df.loc[html_to_df["Week"] != "Week"]
+
+        rename_dt = {
+            "PtsW": "pts_winner",
+            "PtsL": "pts_loser",
+            "YdsW": "yards_winner",
+            "YdsL": "yards_loser",
+            "TOW": "turnovers_winner",
+            "TOL": "turnovers_loser",
+            "Winner/tie": "winner_tie",
+            "Loser/tie": "loser_tie",
+            "Unnamed: 5": "home_away",
+            "Unnamed: 7": "boxscore",
+        }
+
+        html_to_df.rename(columns=rename_dt, inplace=True)
+
+        html_to_df.reset_index(drop=True, inplace=True)
+
+        return html_to_df
 
     def get_table(self, soup_object):
         html_table = soup_object.select("table#games")[0]
@@ -116,10 +193,44 @@ class Scraping:
 
         return game_week_tbl
 
-    def scrape_dot_main(self, team_abbr, year):
-        soup = self.make_soup(team_abbr=team_abbr, year=year)
+    def scrape_schedule_dot_main(self, team_abbr=None):
+        soup = self.make_soup(team_abbr=team_abbr)
+        get_tm_abbrev = self.get_boxscore_url(soup_object=soup)
+        get_schedule_tbl = self.get_schedule(soup_object=soup)
+
+        get_schedule_tbl["home_team"] = get_tm_abbrev
+        get_schedule_tbl = get_schedule_tbl.loc[get_schedule_tbl["Date"] != "Playoffs"]
+
+        abbrev_tbl = DataLoader().load_abbrev_table()
+
+        get_schedule_tbl["winner_abbrev"] = get_schedule_tbl["winner_tie"].map(
+            abbrev_tbl
+        )
+        get_schedule_tbl["loser_abbrev"] = get_schedule_tbl["loser_tie"].map(abbrev_tbl)
+        get_schedule_tbl["home_team_name"] = get_schedule_tbl["home_team"].map(
+            abbrev_tbl
+        )
+
+        get_schedule_tbl["winner_home_away"] = np.where(
+            get_schedule_tbl["home_team_name"] == get_schedule_tbl["winner_tie"],
+            "home_wins",
+            "away_wins",
+        )
+        get_schedule_tbl["home_team_wins"] = np.where(
+            get_schedule_tbl["home_team_name"] == get_schedule_tbl["winner_tie"], 1, 0
+        )
+
+        find_away_team = self.counter_helper(data_object=get_schedule_tbl)
+        get_schedule_tbl["away_team_name"] = find_away_team
+        get_schedule_tbl["nfl_season"] = self.nfl_year
+        get_schedule_tbl.columns = get_schedule_tbl.columns.str.lower()
+
+        return get_schedule_tbl
+
+    def scrape_dot_main(self, team_abbr):
+        soup = self.make_soup(team_abbr=team_abbr)
         tmp_tbl = self.get_table(soup_object=soup)
         tmp_tbl = self.format_table(table_object=tmp_tbl)
         tmp_tbl["nfl_team"] = team_abbr
-        tmp_tbl["nfl_season"] = year
+        tmp_tbl["nfl_season"] = self.nfl_year
         return tmp_tbl
