7cfe1100103c75ddb84f1d07f51d8f00e13d23ac
Jonathan Armitage <jarmitage@developertown.com>
2022-02-21 13:19:41 -0500

    Add more documentation

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -51,6 +51,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "alabaster"
+html_theme_options = {"rightsidebar": "true", "relbarbgcolor": "black"}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
