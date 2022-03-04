62ba651fd982109202ff2c48214ecf59b76ffac9
Jonathan Armitage <jarmitage@developertown.com>
2022-03-04 10:44:31 -0500

    add inference methods

--- nflMatchupPredictor/Models/LinearModels.py
+++ nflMatchupPredictor/Models/LinearModels.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 # import numpy as np
@@ -35,11 +36,11 @@ class LinearModels:
         design_matrix : DataFrame
             _description_
         response : Vector
-            _description_
+            the outcome of interest
 
         Returns
         -------
-        _type_
+        Logistic Regression (sklearn)
             _description_
         """
         clf_lr = self.define_clf()
@@ -52,7 +53,7 @@ class LinearModels:
 
         Parameters
         ----------
-        model_object : _type_
+        model_object : Logistic Regression (sklearn)
             _description_
 
         Returns
@@ -65,27 +66,52 @@ class LinearModels:
         wgts = model_object.coef_
         bias_tbl = pd.DataFrame(bias, columns=["bias"])
         wgts_tbl = pd.DataFrame(wgts, columns=model_object.feature_names_in_)
+
         return pd.concat([bias_tbl, wgts_tbl], axis=1)
 
-    def make_inference(self, model_object, data):
+    def make_random_sample_test(self, data_object, nbr_sample=2):
+        tmp_sample = data_object.sample(n=nbr_sample).reset_index(drop=True)
+        # tmp_sample_answer = tmp_sample["home_team_wins"]
+        return tmp_sample
+
+    def make_inference(self, model_object, data_object):
         """
         Produce estimates for data.
 
         Parameters
         ----------
-        model_object : _type_
+        model_object : Logistic Regression (sklearn)
             _description_
-        data : DataFrame or array-like object
+        data_object : DataFrame or array-like object
             _description_
 
         Returns
         -------
-        Array
-            _description_
-        Array
-            _description_
-        """
-        pred_cls = model_object.predict(data)
-        pred_probs = model_object.predict_proba(data)
+        Dict
+            Contains the following:
+                1. predicted class --> array
+                2. predicted probabilities --> array
+                3. standard deviation of the predicted probabilities --> array
+                4. absolute difference of the predicted probabilities --> array
 
-        return pred_cls, pred_probs
+        Notes
+        -----
+        Calculating and returning the standard deviation and absolute difference of the
+        predicted probabilities allows for further underestanding of the models decision
+        boundary.
+        """
+        pred_cls = model_object.predict(data_object)
+        pred_probs = model_object.predict_proba(data_object)
+
+        std_diff = np.std(pred_probs, axis=1)
+        abs_diff = np.abs(np.diff(pred_probs, axis=1))
+        abs_diff = abs_diff.reshape(std_diff.shape)
+
+        infer_dt = {
+            "pred_cls": pred_cls,
+            "pred_probs": pred_probs,
+            "std_diff": std_diff,
+            "abs_diff": abs_diff,
+        }
+
+        return infer_dt
